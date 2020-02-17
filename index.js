993f473b17e39237b8a953d0353f9f5b719a59e4
Ghostwill20 <carwilli21@gmail.com>
2020-02-17 16:02:10 -0500

    added to website

---
+++ index.js
@@ -0,0 +1,25 @@
+//my button on my webpage event listener
+
+
+document.getElementById("button1").addEventListener("click", function() {
+    alert("SELF DESTRUCTION ACTIVATED!!!!!!!!!!!!!!!!"); }
+  );
+
+  document.getElementById("button2").addEventListener("click", function() {
+    alert("the countdown squence has begun"); }
+  );
+  //end of this
+  function cal(){
+    var num1 = document.getElementById("num1").value
+    var num2 = document.getElementById("num2").value
+  } return(num1,num2);
+
+var value = document.getElementById()
+cal(12, 4);
+  = cal(50, 8);
+  = cal (60, 9);
+   cal(5,5);
+  console.log();
+
+
+
