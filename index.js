9d6d198440c544af4940870b180e9fb21a0b80e7
Ghostwill20 <carwilli21@gmail.com>
2020-03-12 16:14:00 -0400

    update

---
+++ index.js
@@ -0,0 +1,76 @@
+// projects html //
+
+
+//answers
+ answers0 = "Augusta";
+ qusetions= "26";
+ question1 = "Patriots";
+  = "Ps4";
+ a = "Grovetown";
+ questions5 = "Tampa";
+//
+
+//this is the function to check answers
+function checkAnswers() {
+
+    question0 = document.getElementById("question0").value;
+    question1 = document.getElementById("question1").value;
+    question2 = document.getElementById("question2").value;
+    question3 = document.getElementById("question3").value;
+    question4 = document.getElementById("question4").value;
+    question5 = document.getElementById("question5").value;
+
+    if(question0 === "Augusta"){
+        alert("pass");
+    }
+    // want this to run if true works
+    else if(question0!== "Augusta");
+    document.write("You Failed")
+    // want this to run if not true works
+
+    if(qusetion1 === "26"){
+        alert ("pass");
+    }
+
+    else if (qusetion1 !== "26");
+    document.write("You Failed")
+    
+    if(question2 === "Patriots"){
+        alert("pass")
+    }
+
+    else if (question2 !== "Patriots");
+    document.write("You Failed")
+   
+    if( question3 === "Ps4"){
+        alert("pass")
+    }
+    
+    else if ( question3 !== "Ps4");
+    document.write("You Failed")
+    
+    if(question4 === "Grovetown"){
+        alert("pass")
+    }
+   
+    else if (aquestion4 !== "Grovetown");
+    document.write("You Failed")
+
+    if(questions5 === "Tampa"){
+        alert("pass")
+    }  
+    
+    else if (questions5 !== "Tampa");
+    document.write("You Failed")
+}
+
+
+
+
+
+
+
+// these are refrences to my id on my html page// let Quiz = documentgetElementById('quiz')
+ 
+
+
