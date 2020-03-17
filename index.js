6d1c9f00b3ba9034061e006e1c96436c4ec2c880
Ghostwill20 <carwilli21@gmail.com>
2020-03-17 14:43:48 -0400

    update

--- index.js
+++ index.js
@@ -3,66 +3,69 @@
 
 //answers
  answers0 = "Augusta";
- qusetions= "26";
- question1 = "Patriots";
-  = "Ps4";
- a = "Grovetown";
- questions5 = "Tampa";
+ answers1 = "26";
+ answers2 = "Patriots";
+ answers3 = "Ps4";
+ answers4 = "Grovetown";
+ answers5 = "Tampa";
 //
 
 //this is the function to check answers
 function checkAnswers() {
 
-    question0 = document.getElementById("question0").value;
-    question1 = document.getElementById("question1").value;
-    question2 = document.getElementById("question2").value;
-    question3 = document.getElementById("question3").value;
-    question4 = document.getElementById("question4").value;
-    question5 = document.getElementById("question5").value;
+    answers0 = document.getElementById("question0").value;
+    answers1= document.getElementById("question1").value;
+    answers2 = document.getElementById("question2").value;
+    answers3 = document.getElementById("question3").value;
+    answers4 = document.getElementById("question4").value;
+    answers5 = document.getElementById("question5").value;
 
-    if(question0 === "Augusta"){
+    if(answers0 === "Augusta"){
         alert("pass");
     }
     // want this to run if true works
-    else if(question0!== "Augusta");
-    document.write("You Failed")
+    else if(answers0 !== "Augusta"){
+    document.write("You Failed")}
     // want this to run if not true works
 
-    if(qusetion1 === "26"){
+    if(answers1 === "26"){
         alert ("pass");
     }
 
-    else if (qusetion1 !== "26");
-    document.write("You Failed")
+    else if (answers1 !== "26"){
+    document.write("You Failed")}
     
-    if(question2 === "Patriots"){
+    if(answers2 === "Patriots"){
         alert("pass")
     }
 
-    else if (question2 !== "Patriots");
-    document.write("You Failed")
+    else if (answers2 !== "Patriots"){
+    document.write("You Failed")}
    
-    if( question3 === "Ps4"){
+    if( answers3 === "Ps4"){
         alert("pass")
     }
     
-    else if ( question3 !== "Ps4");
-    document.write("You Failed")
+    else if ( answers3 !== "Ps4"){
+    document.write("You Failed")}
     
-    if(question4 === "Grovetown"){
+    if(answers4 === "Grovetown"){
         alert("pass")
     }
    
-    else if (aquestion4 !== "Grovetown");
-    document.write("You Failed")
+    else if (answers4 !== "Grovetown"){
+    document.write("You Failed")}
 
-    if(questions5 === "Tampa"){
+    if(answers5 === "Tampa"){
         alert("pass")
     }  
     
-    else if (questions5 !== "Tampa");
-    document.write("You Failed")
+    else if (answers5 !== "Tampa");
+  {  document.write("You Failed")
 }
+}
+
+
 
 
 
